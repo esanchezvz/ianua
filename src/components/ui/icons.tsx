f0f92aff1d7c2b707ceb86c63ce39438f9369229
Esteban Sánchez <esteban.sanvaz@gmail.com>
2023-05-11 06:59:52 -0600

    feat: checkbox and radio components

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -130,3 +130,22 @@ export function StarIcon(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function Circle(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      {...props}
+    >
+      <circle cx="12" cy="12" r="10" />
+    </svg>
+  )
+}
